ef7cf6fa006c404ff90925e6757e08417b9f73c4
Christopher Gwizdala <christopher.gwizdala@gmail.com>
2026-02-10 16:46:21 -0500

    Initial Commit

---
+++ cali/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cali
+//
+//  Created by Christopher Gwizdala on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
